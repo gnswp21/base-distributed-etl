75f47f9c2058bd853c0731e9386e8e38cdf48dd2
family <gnswp21@gmail.com>
2024-05-28 12:32:10 +0900

    REFORMAT code reformat - 코드 분리 - 객체지향

--- src/main/java/com/example/load/Tester.java
+++ src/main/java/com/example/load/Tester.java
@@ -1,5 +1,6 @@
 package com.example.load;
 
+import com.example.processor.SparkProcessor;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -10,56 +11,19 @@ import java.util.concurrent.TimeoutException;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        SparkSession spark = SparkSession.builder()
-                .master("yarn").appName("test").getOrCreate();
+        // create spark session master:yarn, appNmae:test
+        SparkProcessor sp = new SparkProcessor("test");
 
-        String filePath = "hdfs://mycluster/data/webtoon-data.csv";
+        // read dataframe from HDFS
+        Dataset<Row> df = sp.readDF("hdfs://mycluster/data/webtoon-data.csv");
 
-        // CSV 파일 읽기
-        Dataset<Row> df = spark.read().format("csv")
-                .option("header", "true")
-                .option("inferSchema", "true")
-                .load(filePath);
-        // 데이터프레임의 상위 5개 행 출력
-        df.show(5);
+        // svae DF to hdfs
+        sp.saveDF2Hdfs(df);
 
-        // 카프카로 전송
-        Dataset<Row> jsonDF = df.selectExpr("CAST(id AS STRING) as key", "to_json(struct(*)) AS value");
-
-        // Kafka로 데이터프레임을 전송
-        try {
-            jsonDF
-                .write()
-                .format("kafka")
-                .option("kafka.bootstrap.servers", "mykafka1:9092")
-                .option("topic", "webtoon-topic")
-                .option("checkpointLocation", "/tmp/kafka-checkpoints")
-                .save();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-
-//
-//        // 디렉터리로 파일로 저장
-//        df.write().format("csv").mode("overwrite").option("sep", "\t")
-//                .save("/data/webtoon-data-copy.csv");
-
-//        //// 디렉토리에서 CSV 파일 읽기
-//                String readPath = "hdfs://mycluster/data/webtoon-data-copy.csv";
-//                Dataset<Row> df2 = spark.read()
-//                    .format("csv")
-//                    .option("header", "true")
-//                    .option("inferSchema", "true")
-//                    .option("sep", "\t")
-//                    .load(readPath);
-//
-//        df2.show(5);
-        // 세션 종료
-        spark.stop();
+        // send DF to kafka brokers as topic
+        sp.sendDF2Kafka(df);
+        // stop spark session
+        sp.stopSparkSession();
 
     }
 }
