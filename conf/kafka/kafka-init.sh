b22f256ee210bf757e2f75e4430eb7bceb65645d
family <gnswp21@gmail.com>
2024-05-28 16:23:40 +0900

    FEAT ssh config add StrictHostKeyChecking=no for skipping enter "yes"

--- conf/kafka/kafka-init.sh
+++ conf/kafka/kafka-init.sh
@@ -1,22 +1,10 @@
-# For zookeeper1
-ssh mykafka1 echo '"brokers.id=1" >> /root/kafka/config/server.properties'
-ssh mykafka1 mkdir -p /var/lib/zookeeper
-ssh mykafka1 echo '"1" > /var/lib/zookeeper/myid'
-# For zookeeper2
-ssh mykafka2 echo '"brokers.id=2" >> /root/kafka/config/server.properties'
-ssh mykafka2 mkdir -p /var/lib/zookeeper
-ssh mykafka2 echo '"2" > /var/lib/zookeeper/myid'
+for i in 1 2 3; do
+  host="mykafka$i"
 
-# For zookeeper3
-ssh mykafka3 echo '"brokers.id=3" >> /root/kafka/config/server.properties'
-ssh mykafka3 mkdir -p /var/lib/zookeeper
-ssh mykafka3 echo '"3" > /var/lib/zookeeper/myid'
+  ssh -o StrictHostKeyChecking=no $host echo "\"brokers.id=$i\" >> /root/kafka/config/server.properties"
+  ssh $host mkdir -p /var/lib/zookeeper
+  ssh $host echo "\"$i\" > /var/lib/zookeeper/myid"
 
-# run
-ssh mykafka1 "sh /root/kafka/bin/zookeeper-server-start.sh -daemon /root/kafka/config/zookeeper.properties"
-ssh mykafka2 "sh /root/kafka/bin/zookeeper-server-start.sh -daemon /root/kafka/config/zookeeper.properties"
-ssh mykafka3 "sh /root/kafka/bin/zookeeper-server-start.sh -daemon /root/kafka/config/zookeeper.properties"
-
-ssh mykafka1 "sh /root/kafka/bin/kafka-server-start.sh -daemon /root/kafka/config/server.properties"
-ssh mykafka2 "sh /root/kafka/bin/kafka-server-start.sh -daemon /root/kafka/config/server.properties"
-ssh mykafka3 "sh /root/kafka/bin/kafka-server-start.sh -daemon /root/kafka/config/server.properties"
+  ssh $host "sh /root/kafka/bin/zookeeper-server-start.sh -daemon /root/kafka/config/zookeeper.properties"
+  ssh $host "sh /root/kafka/bin/kafka-server-start.sh -daemon /root/kafka/config/server.properties"
+done
--- conf/namenodes/cluster-restart.sh
+++ conf/namenodes/cluster-restart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ssh namenode1 "zkServer.sh start"
-ssh namenode2 "zkServer.sh start"
-ssh resourcemanager "zkServer.sh start"
+ssh -o StrictHostKeyChecking=no namenode1 "zkServer.sh start"
+ssh -o StrictHostKeyChecking=no namenode2 "zkServer.sh start"
+ssh -o StrictHostKeyChecking=no resourcemanager "zkServer.sh start"
 ssh namenode1 start-dfs.sh
 ssh resourcemanager start-yarn.sh
 
